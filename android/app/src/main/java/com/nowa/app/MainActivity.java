570542b7150031d59aab43c297b3f916c9804656
naimintun1 <buno5928@gmail.com>
2026-09-18 17:33:19 +0630

    Fix Android back button navigation

--- android/app/src/main/java/com/nowa/app/MainActivity.java
+++ android/app/src/main/java/com/nowa/app/MainActivity.java
@@ -1,5 +1,19 @@
 package com.nowa.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onBackPressed() {
+        WebView webView = this.bridge.getWebView();
+
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
